77ed985dfa3262fa37dd98a9d2cb8a6ec0ab04b4
Elias Consalvo <eliasconsalvo@gmail.com>
2025-01-05 19:28:42 -0500

    Slider

--- coldwater-app/src/components/Carousel.tsx
+++ coldwater-app/src/components/Carousel.tsx
@@ -1,5 +1,5 @@
 import Slider from 'react-slick';
-
+import { products } from '../../public/products';
 const Carousel = () => {
   const settings = {
     dots: true,
@@ -27,7 +27,23 @@ const Carousel = () => {
     <div>
       <h2>Product Slider</h2>
       <Slider {...settings}>
-      
+        {products.map((product) => {
+          return (
+            <div style={{ border: "solid 1px grey" }}>
+              <div >Name: {product.productName}</div>
+              <div >Category: {product.category}</div>
+              <div >Price: {product.price}</div>
+              <img
+                src={product.imageMain}
+                onError={(e) => {
+                  const target = e.target as HTMLImageElement;
+                  target.src = "public/fallback-image.png";
+                }}
+                style={{ maxHeight: "12rem" }}
+              />
+            </div>
+          )
+        })}
       </Slider>
     </div>
   );
