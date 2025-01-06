f6b876db50576505c041d5c553079f7f71e8fc61
Elias Consalvo <eliasconsalvo@gmail.com>
2025-01-05 19:10:42 -0500

    Initial commit

---
+++ coldwater-app/src/components/Carousel.tsx
@@ -0,0 +1,36 @@
+import Slider from 'react-slick';
+
+const Carousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <h2>Product Slider</h2>
+      <Slider {...settings}>
+      
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
